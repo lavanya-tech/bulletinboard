80506c5aef89c061e97ab0b2cf3abc8b798325a9
lavanya <jureddilavanya261@gmail.com>
2022-06-01 10:46:29 +0530

    added Loading component to fallback

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home.js';
 // import Signin from './pages/signin.js';
 // import Signup from './pages/signup.js';
+import { Navigate } from 'react-router-dom';
 import React, { Suspense } from 'react';
 import Todaysposts from './pages/todaysposts';
 import Navigation from './components/navigation';
-
+import Loading from './components/loading';
 const Signin = React.lazy(()=>import('./pages/signin'));
 const Signup = React.lazy(()=>import('./pages/signup'));
 
@@ -15,9 +16,9 @@ function App() {
   return (
     <div className="App">
       <Navigation></Navigation>
-      <Suspense>
+      <Suspense fallback={<Loading></Loading>}>
       <Routes>
-        <Route exact path='/' element={<Home></Home>}></Route>
+        <Route exact path='/' element={<Navigate replace to='/home' />}></Route>
         <Route exact path='/home' element={<Home></Home>}></Route>
         <Route exact path='/signin' element={<Signin></Signin>}></Route>
         <Route exact path='/signup' element={<Signup></Signup>}></Route>
---
+++ src/components/loading.js
@@ -0,0 +1,6 @@
+import { Spinner } from "react-bootstrap";
+const Loading = () => {
+    return (<Spinner animation="border" variant="primary" />);
+}
+
+export default Loading;
