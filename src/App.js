766dbf9f83cdede3aa0675cc1fecd90ef9992e1f
lavanya <jureddilavanya261@gmail.com>
2022-06-04 01:24:57 +0530

    added http cookie so the browser could remember the user

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
-import React, { Suspense, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import Visitor from './components/visitor';
 import User from './components/user';
 function App() {
   const [loggedIn,setLoggedIn] = useState(0);
   const SigninHandler = (event) => {
     event.preventDefault();
+    localStorage.setItem('logged','1');
     setLoggedIn(1);
-  }
+    
+  } 
   const Onsignout = () => {
+    localStorage.setItem('logged','0');
     setLoggedIn(0);
   }
+  useEffect(()=>{
+    if (localStorage.getItem("logged") === "1") {
+      setLoggedIn(1);
+    } 
+  });
+  
   if(loggedIn === 0)
   return (<Visitor SigninHandler={SigninHandler}/>);
   else
